db4abec78936d8dfe5e69079b7d3259218346bdb
Joseph DAngelo <joseph.j.dangelo@gmail.com>
2015-01-31 18:00:30 -0500

    Added report page, navigation, and routing

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-angular.module('debt-calculator',[]);
+/*
+	app.js
+
+	Purpose: Defines the angular application and its dependencies
+*/
+
+angular.module('debt-calculator',['mgcrea.ngStrap', 'ngRoute'])
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/accounts', {
+				templateUrl: 'account/account-list/account-list.html',
+				controller: 'AccountListController'
+			})
+			.when('/reports', {
+				templateUrl: 'report/report.html',
+				controller: 'ReportController'
+			})
+			.otherwise({ redirectTo: '/accounts' });
+		});
---
+++ src/common/nav/nav-controller.js
@@ -0,0 +1,9 @@
+var navController = function( $scope, $location ) {
+	// Returns whether the provided path is the current route in the application
+	$scope.isCurrentLocation = function( path ){
+		return $location.path() == path;
+	};
+};
+
+angular.module( 'debt-calculator' )
+	.controller( 'NavController', navController );
