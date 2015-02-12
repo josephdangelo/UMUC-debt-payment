eea2cfe0c2f666283dca5eaad9f734c3edbf36ff
icebearii <gotsecure@gmail.com>
2015-02-11 22:11:23 -0500

    added totals to the Accounts page

--- src/account/account-list/account-list-controller.js
+++ src/account/account-list/account-list-controller.js
@@ -1,6 +1,56 @@
 var accountListController = function( $scope, AccountFactory ) {
 	$scope.accounts = AccountFactory.accounts;
-};
+
+//$scope.totalHouse = 45;//works and display 45 on list page
+$scope.addNew = "Totals";
+
+//works finally
+//$scope.totalHouse = $scope.accounts[1].name;
+
+$scope.blendedAPR = function () {
+	$scope.answer0 = 0;
+	for(var i=0, len=$scope.accounts.length; i < len; ++i)
+
+	{
+    	$scope.answer0 += Number($scope.accounts[i].APR);
+	}
+
+ $scope.totalHouse0 = $scope.answer0 / $scope.accounts.length;
+ return $scope.totalHouse0;
+
+}; //end addNew function
+
+
+$scope.totalBalance = function () {
+	$scope.answer1 = 0;
+	for(var i=0, len=$scope.accounts.length; i < len; ++i)
+
+	{
+    	$scope.answer1 += Number($scope.accounts[i].balance);
+	}
+
+ $scope.totalHouse1 = $scope.answer1;
+ return $scope.totalHouse1 ;
+
+}; //end addNew function
+
+
+
+$scope.totalMonthly = function () {
+	$scope.answer2 = 0;
+	for(var i=0, len=$scope.accounts.length; i < len; ++i)
+
+	{
+    	$scope.answer2 += Number($scope.accounts[i].payment);
+	}
+
+ $scope.totalHouse2 = $scope.answer2;
+ return $scope.totalHouse2 ;
+
+}; //end addNew function
+
+
+}; //end controller accountListController
 
 angular.module( 'debt-calculator' )
 	.controller( 'AccountListController', accountListController );
