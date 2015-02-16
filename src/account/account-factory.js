7ca2b29e586e33a565f46df9057742ea9ddd6b09
yunjungoh <yunjungoh@yahoo.com>
2015-02-16 06:04:55 -0500

    Changed/added accounts
    Changed/added accounts for a temporary monthly report for testing.

--- src/account/account-factory.js
+++ src/account/account-factory.js
@@ -6,8 +6,11 @@
 var accountFactory = function(){
 	var factory = {};
 
-	factory.accounts = [{ name: 'Bank of America Credit Card', balance: 4000, APR: 3.4, payment: 175 },
-						{ name: 'Citi Credit Card', balance: 2000, APR: 7.4, payment: 75 }];
+	factory.accounts = [{ name: 'Bank of America Credit Card', balance: 2000, APR: 20, payment: 100 },
+				{ name: 'Citi Credit Card', balance: 3000, APR: 16, payment: 100 },
+				{ name: 'American Airlines Credit Card', balance: 4000, APR: 12, payment: 100 },
+				{ name: 'Ford Explorer Auto Loan', balance: 20000, APR: 7, payment: 400 },
+				{ name: 'Mortgage', balance: 200000, APR: 3, payment: 1200 }];
 
 	return factory;
 };
