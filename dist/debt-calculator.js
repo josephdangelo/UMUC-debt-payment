ba25cea10ca8a5a952700153234bebeadf3e4ccd
ArmintaJenkins <arminta.jenkins@gmail.com>
2015-02-18 20:12:58 -0500

    Current updated files acount-list, report

--- dist/debt-calculator.js
+++ dist/debt-calculator.js
@@ -142,7 +142,7 @@ var accountListController = function( $scope, AccountFactory ) {
 		$scope.newAccount = selectedAccount;
 
 		$scope.deleteAccount(account);
-
+	//	AccountFactory.editAccount ( account );
 	};
 
 	$scope.deleteAccount = function( account ) {
