04d6ae1e3f90c6c628bb8fbf744a9fd4ed83833b
mdespang <brainstrainz@gmail.com>
2015-02-19 18:16:46 -0500

    Revert "How to Use add embeded PDF (inop)"
    This reverts commit 6c6246b5dcbba7e9be9ca898303ce2ae5993fb4f.

--- src/account/account-list/account-list-controller.js
+++ src/account/account-list/account-list-controller.js
@@ -50,6 +50,7 @@ var accountListController = function( $scope, AccountFactory ) {
 		$scope.newAccount = selectedAccount;
 
 		$scope.deleteAccount(account);
+	//	AccountFactory.editAccount ( account );
 	};
 
 	$scope.deleteAccount = function( account ) {
